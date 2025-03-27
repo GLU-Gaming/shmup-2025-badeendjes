8c21798745a379f505b9ba301655366370082f9d
theallwatcher <samuelaiden.nap@gmail.com>
2025-03-27 11:03:10 +0100

    angler af

--- Assets/Developers/Sam/SamScripts/angler/anglerMain.cs
+++ Assets/Developers/Sam/SamScripts/angler/anglerMain.cs
@@ -1,16 +1,92 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class anglerMain : enemybase
 {
 
-    public void OnTriggerEnter(Collider other)
+    [SerializeField] private float resetTime = 3.0f;
+    [SerializeField] private float anglerSpeed = 1.0f;
+    [SerializeField] private float timer = 0.0f;
+
+    Vector3 idlePosition;
+
+    private enum EnemyState {
+        Idle,
+        Agro,
+        Return
+    };
+
+    private enum Direction
     {
+        Left = -1,
+        Right = 1
+    }
+
+    private int currentState = (int) EnemyState.Idle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && currentState == (int) EnemyState.Idle)
+        {
+            Debug.Log("Trigger!");
+            currentState = (int)EnemyState.Agro;
+        }
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        timer = resetTime;
+        idlePosition = transform.position;
+    }
+
+    private void IdleEnemyState()
+    {
+        base.Update();
+    }
+
+    private void ReturnEnemyState()
+    {
+        if (transform.position.x < idlePosition.x)
+        {
+            transform.position += new Vector3((int)Direction.Right * anglerSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            currentState = (int) EnemyState.Idle;
+        }
+    }
+
+    private void AgroEnemyState()
+    {
+        // Reset enemy state to idle when timer is over. To do: or when enemy hits the player
+        if (timer < 0.0f)
+        {
+            currentState = (int) EnemyState.Return;
+            timer = resetTime;
+            return;
+        }
+        transform.position += new Vector3((int) Direction.Left * anglerSpeed * Time.deltaTime, 0, 0);
+        timer -= Time.deltaTime;
+    }
+
+    //public override void Update()
+    //{
+    //    switch (currentState)
+    //    {
+    //        case (int)EnemyState.Idle:
+    //            IdleEnemyState();
+    //            break;
+    //        case (int)EnemyState.Agro:
+    //            AgroEnemyState();
+    //            break;
+    //        case (int)EnemyState.Return:
+    //            ReturnEnemyState();
+    //            break;
+    //        default:
+    //            break;
+    //    }
         
-
-    }
-
-    public void biteForward() {
-
-    }
+    //}
 
 }
