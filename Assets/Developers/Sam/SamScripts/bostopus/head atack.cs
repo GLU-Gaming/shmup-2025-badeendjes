a7e75c1ca74af1eedbe7f21412f3133792f484a9
theallwatcher <samuelaiden.nap@gmail.com>
2025-03-31 12:52:54 +0200

    boss start

---
+++ Assets/Developers/Sam/SamScripts/bostopus/head atack.cs
@@ -0,0 +1,26 @@
+using Unity.Mathematics;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class headatack : MonoBehaviour
+{
+    [SerializeField] float fireRate = 0f;
+    private float nextFireTime = 0f;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+    private void Update()
+    {
+        if (Time.time >= nextFireTime)
+        {
+
+            Attack();
+            nextFireTime = Time.time + fireRate;
+
+        }
+    }
+
+    private void Attack() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
